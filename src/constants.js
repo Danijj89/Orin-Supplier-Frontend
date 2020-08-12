01615ae8d54433648db268b003720f5e6f4e8d99
Danijj89 <danijj89@gmail.com>
2020-08-12 15:57:56 -0400

    added env variables for dynamic server uri

--- src/constants.js
+++ src/constants.js
@@ -3,10 +3,11 @@ import { ENGLISH } from './languages/english';
 // The language package used for the application
 export const LANGUAGE = ENGLISH;
 
-const LOCAL_SERVER_URI = 'http://localhost:5000/api';
-const SERVER_URI = LOCAL_SERVER_URI;
+const SERVER_URI = process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_REMOTE_SERVER_URI
+    : process.env.REACT_APP_LOCAL_SERVER_URI;
 
 export const AXIOS_REQUEST_CONFIG = {
-    baseURL: SERVER_URI
+    baseURL: `${SERVER_URI}/api`
 };
 
