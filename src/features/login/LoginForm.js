decdee205469917f5bf76e33c4fcb305f72757d7
Danijj89 <danijj89@gmail.com>
2020-08-14 18:40:21 -0400

    added autofocus on email input field

--- src/features/login/LoginForm.js
+++ src/features/login/LoginForm.js
@@ -19,6 +19,7 @@ export default function LoginForm({ onSignInClick }) {
                     type="email"
                     value={emailField}
                     onChange={onEmailChange}
+                    autoFocus
                 />
             </div>
             <div className="form-group">
