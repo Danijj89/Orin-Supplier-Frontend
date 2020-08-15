94ea455eb783e052dc9c935dc42c09dcb29b0c9d
Danijj89 <danijj89@gmail.com>
2020-08-14 22:44:11 -0400

    added app defaults to store

--- src/features/home/slice.js
+++ src/features/home/slice.js
@@ -3,28 +3,26 @@ import { createSlice } from '@reduxjs/toolkit';
 //TODO Might want to remove these initial states once the app is complete
 const initialState = {
     user: JSON.parse(sessionStorage.getItem('user')),
-    company: JSON.parse(sessionStorage.getItem('company'))
+    company: JSON.parse(sessionStorage.getItem('company')),
+    defaults: JSON.parse(sessionStorage.getItem('defaults'))
 };
 
 const homeSlice = createSlice({
     name: 'home',
     initialState,
     reducers: {
-        setCurrentUserInfo: (state, action) => {
-            let { company, ...userWithoutCompany } = action.payload
-            state.user = userWithoutCompany;
+        setSessionInfo: (state, action) => {
+            const { user, company, defaults } = action.payload
+            state.user = user;
             state.company = company;
+            state.defaults = defaults;
         }
     }
 });
 
-export const selectCurrentUser = state => {
-    let { user } = state.home;
-    if (!user) user = JSON.parse(sessionStorage.getItem('user'));
-    return user;
-};
+export const selectCurrentUser = state => state.home.user;
 export const selectCurrentCompany = state => state.home.company;
 
-export const { setCurrentUserInfo } = homeSlice.actions;
+export const { setSessionInfo } = homeSlice.actions;
 
 export default homeSlice.reducer;
