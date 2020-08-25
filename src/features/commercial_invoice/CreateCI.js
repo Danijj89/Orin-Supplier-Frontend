a0cb8440cfa9c9c810e7e8a952656b5fa8ec1288
Danijj89 <danijj89@gmail.com>
2020-08-25 19:53:12 +0200

    added default style to document stepper component and refactored createCI component

--- src/features/commercial_invoice/CreateCI.js
+++ src/features/commercial_invoice/CreateCI.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectCurrentCompany } from '../home/slice.js';
 import { fetchCIOptions } from './duck/thunks.js';
 import { selectCIActiveStep, selectCISteps } from './duck/selectors.js';
-import { makeStyles } from '@material-ui/core/styles';
 import DocumentStepper from '../shared/DocumentStepper.js';
 import { Container, Typography } from '@material-ui/core';
 import { LANGUAGE } from '../../constants.js';
@@ -16,15 +15,7 @@ import OrderService from '../orders/services.js';
 
 const { title } = LANGUAGE.commercialInvoice.createCI;
 
-const useStyles = makeStyles({
-    stepper: {
-        backgroundColor: 'transparent',
-        padding: '24px 25%'
-    }
-});
-
 export default function CreateCI() {
-    const classes = useStyles();
     const dispatch = useDispatch();
     const { _id } = useSelector(selectCurrentCompany);
     const steps = useSelector(selectCISteps);
@@ -52,7 +43,7 @@ export default function CreateCI() {
 
     return (
         <Container>
-            <DocumentStepper styles={classes.stepper} steps={steps} activeStep={activeStep} />
+            <DocumentStepper steps={steps} activeStep={activeStep} />
             <Typography variant="h5">{title}</Typography>
             <hr/>
             {currOrder && activeStep === 0 && <CreateCIDetailsForm order={currOrder}/>}
--- src/features/shared/DocumentStepper.js
+++ src/features/shared/DocumentStepper.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    stepper: {
+        backgroundColor: 'transparent',
+        padding: '24px 25%'
+    }
+});
 
 export default function DocumentStepper({ activeStep, steps, styles }) {
-
+    const classes = useStyles();
     const renderedSteps = steps.map((label) => (
         <Step key={label}>
             <StepLabel>{label}</StepLabel>
@@ -16,7 +24,7 @@ export default function DocumentStepper({ activeStep, steps, styles }) {
             activeStep={activeStep}
             alternativeLabel
             children={renderedSteps}
-            className={styles}
+            className={styles ? styles : classes.stepper}
         >
         </Stepper>
     )
