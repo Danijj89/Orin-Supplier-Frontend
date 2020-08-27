c37b789d743b171b7d616a8356799825363796a6
Danijj89 <danijj89@gmail.com>
2020-08-27 23:17:26 +0200

    added a helper class to keep track of quantities per unit

---
+++ src/features/shared/classes/UnitCounter.js
@@ -0,0 +1,27 @@
+export default class UnitCounter {
+    #units;
+    #state;
+
+    constructor(units, initialState) {
+        this.#units = units;
+        this.#state = initialState;
+    }
+
+    addUnit(unit, amount) {
+        if (!this.#units.includes(unit))
+            throw new Error(`Unable to add unit. The given unit ${ unit } does not belong to this counter`);
+        this.#state.hasOwnProperty(unit)
+            ? this.#state[unit] += amount
+            : this.#state[unit] = amount;
+    }
+
+    get state() {
+        return {...this.#state};
+    }
+
+    get toString() {
+        return Object.entries(this.#state).map(([unit, amount]) =>
+            `${amount} ${unit}`
+        ).join(' + ');
+    }
+}
