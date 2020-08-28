61551b76be31ba224737b79799e21ffd4c1b18aa
Danijj89 <danijj89@gmail.com>
2020-08-28 21:14:17 +0200

    fixed UnitCounter not adding values properly

--- src/features/shared/classes/UnitCounter.js
+++ src/features/shared/classes/UnitCounter.js
@@ -1,27 +1,35 @@
 export default class UnitCounter {
-    #units;
-    #state;
+    units;
+    data;
 
-    constructor(units, initialState) {
-        this.#units = units;
-        this.#state = initialState;
+    constructor(units, initialData) {
+        this.units = units;
+        this.data = initialData;
     }
 
     addUnit(unit, amount) {
-        if (!this.#units.includes(unit))
+        if (!this.units.includes(unit))
             throw new Error(`Unable to add unit. The given unit ${ unit } does not belong to this counter`);
-        this.#state.hasOwnProperty(unit)
-            ? this.#state[unit] += amount
-            : this.#state[unit] = amount;
+        this._data.hasOwnProperty(unit)
+            ? this._data[unit] += amount
+            : this._data[unit] = amount;
     }
 
-    get state() {
-        return {...this.#state};
+    subtractUnit(unit, amount) {
+        this.addUnit(unit, amount * -1);
+    }
+
+    get data() {
+        return {...this._data};
     }
 
     get stringRep() {
-        return Object.entries(this.#state).map(([unit, amount]) =>
+        return Object.entries(this.data).map(([unit, amount]) =>
             `${amount} ${unit}`
         ).join(' + ');
+    }
+
+    set data(data) {
+        this._data = {...data};
     }
 }
