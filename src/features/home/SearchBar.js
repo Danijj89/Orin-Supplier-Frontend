df74d4e4dbbe5590761a9c6491df660c66c13586
Danijj89 <danijj89@gmail.com>
2020-08-31 13:52:33 +0200

    refactored SearchBar

--- src/features/home/SearchBar.js
+++ src/features/home/SearchBar.js
@@ -1,28 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Container, TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Search as IconSearch } from '@material-ui/icons';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectAllOrders } from '../orders/duck/slice.js';
 import { LANGUAGE } from '../../constants.js';
+import { fetchOrders } from '../orders/duck/thunks.js';
+import { selectPOStatus } from '../orders/duck/selectors.js';
+
+const { search } = LANGUAGE.home.searchBar;
+
+const useStyles = makeStyles({
+    container: {
+        display: 'flex'
+    },
+    searchButton: {
+        backgroundColor: 'lightgrey'
+    }
+})
 
 export default function SearchBar() {
-    const { search } = LANGUAGE.home.searchBar;
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useHistory();
     const [searchTerm, setSearchTerm] = useState('');
+    const orders = useSelector(selectAllOrders);
+    const refs = orders.map(order => order.poRef);
+    const status = useSelector(selectPOStatus);
 
-    const onSearchTermChange = e => setSearchTerm(e.target.value);
+    const onSearchTermChange = val => setSearchTerm(val);
 
     const onSubmit = () => {
-        console.log(searchTerm);
+        if (refs.includes(searchTerm)) {
+            const poId = orders.find(order => order.poRef === searchTerm)._id;
+            history.push(`/home/orders/${poId}`);
+        }
     }
 
-    return (
-        <div className="d-flex">
-            <div className="input-group input-group-lg">
-                <input
-                    type="text"
-                    className="form-control"
-                    value={searchTerm}
-                    onChange={onSearchTermChange}
-                />
-            </div>
-            <button type="submit" onClick={onSubmit}>{search}</button>
-        </div>
+    const mounted = useRef();
+    useEffect(() => {
+        if (orders.length === 0) {
+            if (mounted.current !== status && status === 'IDLE') {
+                dispatch(fetchOrders());
+                mounted.current = status;
+            }
+        }
+    }, [dispatch, orders, status]);
 
+    return (
+        <Container className={ classes.container }>
+            <Autocomplete
+                freeSolo
+                autoSelect
+                options={ refs }
+                renderInput={ params => (
+                    <TextField
+                        { ...params }
+                        variant="outlined"
+                        InputProps={{...params.InputProps, startAdornment: <IconSearch onClick={onSubmit}/>}}
+                    />
+                ) }
+                onChange={ (_, data) => onSearchTermChange(data) }
+                value={searchTerm}
+                fullWidth
+                size="small"
+                onKeyUp={(e) => {
+                    if (e.key === 'Enter') onSubmit();
+                }}
+            />
+            <Button variant="outlined" className={classes.searchButton} onClick={onSubmit}>{ search }</Button>
+        </Container>
     );
 }
