0c0798c0a561d77e1086e47a209a564c2bff0551
Danijj89 <danijj89@gmail.com>
2020-09-01 11:10:02 +0200

    added shared loader component

---
+++ src/features/shared/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './loader.css';
+
+export default function Loader() {
+
+    return (
+        <div id="loader">
+            <div id="shadow"/>
+            <div id="box"/>
+        </div>
+    )
+}
