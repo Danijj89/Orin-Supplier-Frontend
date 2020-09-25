6c6a7bcf7113817d394284ba3ba98b325c2ced4e
Danijj89 <danijj89@gmail.com>
2020-09-25 15:19:37 +0200

    refactored app route

--- src/features/shared/AppRoute.js
+++ src/features/shared/AppRoute.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import HomeLayout from '../home/HomeLayout.js'
-import LoginLayout from '../login/LoginLayout.js';
-
 
 export default ({ component: Component, isPrivate, ...rest }) => {
-
     const signed = sessionStorage.getItem('token');
-
     if (isPrivate && !signed) {
         return <Redirect to='/login' />
     }
-
     if (!isPrivate && signed) {
         return <Redirect to='/home' />
     }
-
-    const Layout = isPrivate ? HomeLayout : LoginLayout;
-
     return (
-        <Route {...rest} render={ props => (
-            <Layout>
-                <Component {...props} />
-            </Layout>
-        )} />
+        <Route {...rest} render={ props => <Component {...props} /> } />
     )
 }
 
