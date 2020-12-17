aee782fb330d7224d7fa094ff91789c9f513473d
Danijj89 <danijj89@gmail.com>
2020-12-17 16:50:42 +0100

    fixed new users not getting displayed after creation

--- src/features/admin/Users.js
+++ src/features/admin/Users.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectAllCompanies } from './duck/companies/selectors.js';
+import { selectAllCompanies, selectCompaniesMap } from './duck/companies/selectors.js';
 import { LANGUAGE } from '../../app/utils/constants.js';
 import Table from '../shared/components/table/Table.js';
 import Autocomplete from '@material-ui/lab/Autocomplete';
@@ -20,7 +20,8 @@ const {
 const Users = React.memo(function Users() {
     const dispatch = useDispatch();
     const companies = useSelector(selectAllCompanies);
-    const [company, setCompany] = useState(null);
+    const companiesMap = useSelector(selectCompaniesMap);
+    const [companyId, setCompanyId] = useState(null);
     const [isEdit, setIsEdit] = useState(false);
     const [user, setUser] = useState(null);
 
@@ -49,14 +50,14 @@ const Users = React.memo(function Users() {
     ], []);
 
     const rows = useMemo(() => {
-        if (!company) return [];
-        return company.users.map(user => ({
+        if (!companyId) return [];
+        return companiesMap[companyId].users.map(user => ({
             user: user,
             name: user.name,
             email: user.email,
             roles: user.roles.join(', ')
         }));
-    }, [company]);
+    }, [companiesMap, companyId]);
 
     return (
         <Paper>
@@ -71,8 +72,8 @@ const Users = React.memo(function Users() {
                 options={ companies }
                 getOptionLabel={ getCompanyLegalName }
                 getOptionSelected={ (option, value) => option._id === value._id }
-                value={ company }
-                onChange={ (_, newValue) => setCompany(newValue) }
+                value={ companiesMap[companyId] || null }
+                onChange={ (_, newValue) => setCompanyId(newValue._id) }
             />
             <Table
                 rows={ rows }
