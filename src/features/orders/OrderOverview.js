7c55f416ba8fb647ac0d5a63ecbb98f041a97c9c
Danijj89 <danijj89@gmail.com>
2021-02-14 20:42:48 -0500

    refactored SearchBar component and added it to OrderOverview

--- src/features/orders/OrderOverview.js
+++ src/features/orders/OrderOverview.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { LANGUAGE } from 'app/utils/constants.js';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { cleanNewOrder } from './duck/slice.js';
 import { Paper } from '@material-ui/core';
 import ThemedButton from '../shared/buttons/ThemedButton.js';
@@ -9,41 +9,57 @@ import OrdersTable from './OrdersTable.js';
 import { makeStyles } from '@material-ui/core/styles';
 import { CREATE_ANY, CREATE_OWN } from '../admin/utils/actions.js';
 import OrderPermission from '../shared/permissions/OrderPermission.js';
+import SearchBar from 'features/shared/components/SearchBar.js';
+import { selectAllActiveOrders } from 'features/orders/duck/selectors.js';
+import { getOrderURL } from 'features/orders/utils/urls.js';
+import Box from '@material-ui/core/Box';
 
 const { newOrderButtonLabel } = LANGUAGE.order.ordersOverview;
 
 const useStyles = makeStyles((theme) => ({
-    orderOverviewRoot: {
+    container: {
         margin: theme.spacing(2),
     },
-    newOrder: {
-        margin: theme.spacing(2),
-    },
+    topRow: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: theme.spacing(2)
+    }
 }));
 
 const OrderOverview = React.memo(function OrderOverview() {
     const classes = useStyles();
     const dispatch = useDispatch();
     const history = useHistory();
-
+    const orders = useSelector(selectAllActiveOrders);
 
     const onNewOrderClick = () => {
         dispatch(cleanNewOrder());
         history.push('/home/orders/new?step=details');
     };
 
+    const getUrl = useCallback(order => getOrderURL(order._id), []);
+
     return (
-        <Paper className={ classes.orderOverviewRoot }>
-            <OrderPermission action={ [CREATE_ANY, CREATE_OWN] }>
-                <ThemedButton
-                    className={ classes.newOrder }
-                    onClick={ onNewOrderClick }
-                >
-                    { newOrderButtonLabel }
-                </ThemedButton>
-            </OrderPermission>
-            <OrdersTable/>
-        </Paper>
+        <OrderPermission action={ [CREATE_ANY, CREATE_OWN] }>
+            <Paper className={ classes.container }>
+                <Box className={ classes.topRow }>
+                    <ThemedButton
+                        className={ classes.newOrder }
+                        onClick={ onNewOrderClick }
+                    >
+                        { newOrderButtonLabel }
+                    </ThemedButton>
+                    <SearchBar
+                        options={ orders }
+                        getOptionLabel={ order => order.ref }
+                        getOptionSelected={ (order, value) => order._id === value._id }
+                        getUrl={ getUrl }
+                    />
+                </Box>
+                <OrdersTable/>
+            </Paper>
+        </OrderPermission>
     );
 });
 
---
+++ src/features/shared/components/SearchBar.js
@@ -0,0 +1,84 @@
+import React, { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { TextField } from '@material-ui/core';
+import { Search as IconSearch } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import clsx from 'clsx';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        minWidth: 200,
+        maxWidth: 320,
+        backgroundColor: theme.palette.backgroundPrimary.main,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderRadius: 4,
+        borderColor: theme.palette.grey.light,
+        '&.Mui-focused': {
+            borderColor: theme.palette.primary.main,
+        },
+        paddingLeft: theme.spacing(1),
+        paddingRight: theme.spacing(1),
+        paddingTop: theme.spacing(0.2)
+    }
+}));
+
+const SearchBar = React.memo(function SearchBar({ options, getOptionLabel, getOptionSelected, getUrl, className }) {
+    const classes = useStyles();
+    const [searchText, setSearchText] = useState(null);
+    const history = useHistory();
+
+    const onSubmit = useCallback(() => {
+        if (searchText) history.push(getUrl(searchText));
+    }, [history, getUrl, searchText]);
+
+    const onSearchTextChange = useCallback(
+        (_, data) => setSearchText(data),
+        []);
+
+    const onKeyUp = useCallback(e => {
+        if (e.key === 'Enter') onSubmit();
+    }, [onSubmit]);
+
+    return (
+        <Autocomplete
+            options={ options }
+            getOptionLabel={ getOptionLabel }
+            getOptionSelected={ getOptionSelected }
+            renderInput={ (params) => (
+                <TextField
+                    { ...params }
+                    // variant="outlined"
+                    InputProps={ {
+                        ...params.InputProps,
+                        disableUnderline: true,
+                        endAdornment:
+                            <IconButton size="small">
+                                <IconSearch onClick={ onSubmit }/>
+                            </IconButton>,
+                        className: clsx(classes.root, className),
+                    } }
+                />
+            ) }
+            onChange={ onSearchTextChange }
+            value={ searchText }
+            size="small"
+            onKeyUp={ onKeyUp }
+        />
+    );
+}, (prev, next) => {
+    return prev.options !== next.options;
+});
+
+SearchBar.propTypes = {
+    options: PropTypes.array.isRequired,
+    getUrl: PropTypes.func.isRequired,
+    getOptionLabel: PropTypes.func,
+    getOptionSelected: PropTypes.func,
+    className: PropTypes.string
+};
+
+export default SearchBar;
