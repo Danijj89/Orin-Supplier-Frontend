fb5d3587a8a746f6a71ade5d8ca5cf5fae269020
Danijj89 <danijj89@gmail.com>
2021-02-23 22:12:52 -0500

    added authing prod env variables

--- src/app/utils/authing.js
+++ src/app/utils/authing.js
@@ -1,9 +1,11 @@
 import { AuthenticationClient } from 'authing-js-sdk';
 
 export const authing = new AuthenticationClient({
-    appId: process.env.NODE_ENV
+    appId: process.env.NODE_ENV === 'development'
         ? process.env.REACT_APP_DEV_AUTHING_APP_ID
-        : process.env.REACT_APP_DEV_AUTHING_APP_ID,
-    appDomain: process.env.REACT_APP_DEV_AUTHING_APP_DOMAIN,
+        : process.env.REACT_APP_PROD_AUTHING_APP_ID,
+    appDomain: process.env.NODE_ENV === 'development'
+        ? process.env.REACT_APP_DEV_AUTHING_APP_DOMAIN
+        : process.env.REACT_APP_PROD_AUTHING_APP_DOMAIN,
     onError: (code, message, data) => console.log(message)
 });
