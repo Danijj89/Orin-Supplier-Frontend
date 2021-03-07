be4ab286268f4522e2279d488f601026aeb28892
Xiang Zhao <xiangzhao@xiangs-mbp.mynet>
2021-03-07 14:18:01 +0800

    fixed text wrap in docs preview

--- src/features/documents/ChinaExportPreview.js
+++ src/features/documents/ChinaExportPreview.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Typography} from '@material-ui/core';
+import {Grid, Typography} from '@material-ui/core';
 // import {InfoBox, GenerateAddress, GenerateTable, GenerateEqualFooter} from './utils/helpers.js';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 // import { format } from 'date-fns';
-import { LANGUAGE } from 'app/utils/constants.js';
+import {LANGUAGE} from 'app/utils/constants.js';
 
 const {
     ceTitle,
@@ -24,9 +24,9 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-const ChinaExportPreview = React.memo(function ChinaExportPreview({ document }) {
+const ChinaExportPreview = React.memo(function ChinaExportPreview({document}) {
     const classes = useStyles();
-     const {
+    const {
         ref,
         // bol,
         // containerNum,
@@ -51,21 +51,21 @@ const ChinaExportPreview = React.memo(function ChinaExportPreview({ document }) 
         // totalAmount,
         // tradingCountry,
     } = document
-    // console.log(document)
-    // console.log(document.items)
+    console.log(document)
+    console.log(document.items)
     return (
-        <Grid className={ classes.root }>
-            <Grid container direction = "column">
+        <Grid className={classes.root}>
+            <Grid container direction="column">
                 <Grid item><Typography variant="h5">{ceTitle}</Typography></Grid>
                 <Grid item xs={12} className={classes.title}>
-                <Typography variant="h5" align="center">
-                    中华人民共和国海关出口货物报关单
-                </Typography>
+                    <Typography variant="h5" align="center">
+                        中华人民共和国海关出口货物报关单
+                    </Typography>
                 </Grid>
                 <Grid item xs={12} className={classes.title} direction="row"
-                justify="space-between" container>
-                <Typography variant="subtitle1">预录入编号：</Typography>
-                <Typography variant="subtitle1">海关编号：{ref}</Typography>
+                      justify="space-between" container>
+                    <Typography variant="subtitle1">预录入编号：</Typography>
+                    <Typography variant="subtitle1">海关编号：{ref}</Typography>
                 </Grid>
             </Grid>
             <Typography variant="h4">Coming Soon | 在建立中</Typography>
